0ceb1acb7f4b8bb31a428d9bc92277fe8e80d32f
Kira <kira@Kiras-MacBook-Air.local>
2014-07-18 17:27:29 +0800

    add toggle btn

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
 Replicate from site : http://lifelabs.2359studios.com/
-Link Demo : https://de7e1894a89324684d0bd9884e1bf254fda0cd22.googledrive.com/host/0B7x7qAB9kVr5SU5EbmxPMm9NUnc/index.html
+Link Demo : http://thanhnguyen2359.github.io/SingTel
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+;(function($,undefined){
+	$(document).ready(function(){
+		var menus = $('.nav-bar-toggle'),
+			intro = $('.intro'),
+			menuStatus = false;
+		$('.nav-bar-btn').off('click').on('click',function(e){
+			e.preventDefault();
+			if(menuStatus) {
+				intro.delay(1600).removeClass('intro-expand');
+				menus.hide(500);
+				
+			}else{
+				intro.delay(1600).addClass('intro-expand');
+				menus.show(500);			
+			}
+			menuStatus = !menuStatus;
+		});
+	});
+})(window.jQuery);
